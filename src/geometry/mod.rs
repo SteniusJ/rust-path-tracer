64f93b4f73df0f174736c3dc1d1e878ec9804c91
SteniusJ <stenius.patrik@gmail.com>
2026-04-18 22:49:22 +0300

    add cuboid

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -1,4 +1,4 @@
-use crate::{ray, vec3, hittable, materials};
+use crate::{vec3, materials, hittable, ray};
 
 pub struct Triangle<'a, T: materials::Material> {
     pub vertice1: vec3::Vec3,
@@ -33,4 +33,53 @@ impl<'a, T> Triangle<'a, T> where T: materials::Material {
             material
         }
     }
+}
+
+impl<'a, T> hittable::Hittable for Triangle<'a, T> where T: materials::Material {
+    fn hit(ray: &ray::Ray, t_min: f32, t_max: f32, rec: &mut hittable::HitRecord) {
+        // Implement triangle hit functions here
+    }
+}
+
+pub struct Cuboid<'a, T: materials::Material> {
+    pub triangles: *mut Triangle<'a, T>,
+}
+
+impl<'a, T> Cuboid<'a, T> where T: materials::Material {
+    /* Constructs new Cuboid
+     * Returns vector of Triangle
+     *
+     * Placement order of vertices. Face 1,2,3,4 is closer to the observer
+     *
+     *    7________8
+     *   /|       /|
+     * 3/_______4/ |
+     * |  |     |  |
+     * |  5_____|__6
+     * | /      | /
+     * 1/_______2/
+     *
+     */
+    pub fn new(v1: vec3::Vec3, v2: vec3::Vec3, v3: vec3::Vec3, v4: vec3::Vec3, v5: vec3::Vec3, v6: vec3::Vec3, v7: vec3::Vec3, v8: vec3::Vec3, material: &'a T) -> (Cuboid<'a, T>, Vec<Triangle<'a, T>>) {
+        let mut triangles: Vec<Triangle<'a, T>> = Vec::with_capacity(12);
+        triangles.push(Triangle::new(v1, v2, v3, material));
+        triangles.push(Triangle::new(v3, v2, v4, material));
+        triangles.push(Triangle::new(v1, v3, v5, material));
+        triangles.push(Triangle::new(v3, v7, v5, material));
+        triangles.push(Triangle::new(v1, v2, v5, material));
+        triangles.push(Triangle::new(v5, v2, v6, material));
+        triangles.push(Triangle::new(v8, v4, v2, material));
+        triangles.push(Triangle::new(v2, v6, v8, material));
+        triangles.push(Triangle::new(v5, v7, v8, material));
+        triangles.push(Triangle::new(v5, v8, v6, material));
+        triangles.push(Triangle::new(v3, v8, v7, material));
+        triangles.push(Triangle::new(v3, v4, v8, material));
+
+        (
+            Cuboid {
+                triangles: triangles.as_mut_ptr(),
+            },
+            triangles
+        )
+    } 
 }
