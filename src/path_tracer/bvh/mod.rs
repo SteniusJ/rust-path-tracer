7d98943490e8436f35e6e089054dce7ae20e7561
SteniusJ <stenius.patrik@protonmail.com>
2026-06-18 18:48:28 +0300

    bvh finished

--- src/path_tracer/bvh/mod.rs
+++ src/path_tracer/bvh/mod.rs
@@ -51,7 +51,7 @@ impl<'a> BVH<'a> {
         /* Maximum stack size.
          * 100 should be overkill for any reasonable scene.
          */
-        static STACK_SIZE: usize = 10;
+        static STACK_SIZE: usize = 20;
         let mut stack = [0; STACK_SIZE];
         let mut stack_top = 0_i64;
 
@@ -64,13 +64,15 @@ impl<'a> BVH<'a> {
 
         while stack_top > 0 {
             stack_top -= 1;
-            let node = self.nodes[stack[stack_top as usize]];
+            let node_idx = stack[stack_top as usize];
+            let node = self.nodes[node_idx];
 
-            if !intersect_aabb(ray, temp_rec.t, node.aabb_min, node.aabb_max) { continue; }
+            if !intersect_aabb(ray, closest_t, node.aabb_min, node.aabb_max) { continue; }
 
             if node.is_leaf() {
                 for i in 0..node.tri_count {
-                    if self.tris[self.tri_indices[node.first_tri_idx + i]].hit(ray, 0.0001, closest_t, &mut temp_rec) {
+                    let tri_idx = self.tri_indices[node.first_tri_idx + i];
+                    if self.tris[tri_idx].hit(ray, 0.0001, closest_t, &mut temp_rec) {
                         hit = true;
                         if closest_t > temp_rec.t {
                             closest_t = temp_rec.t;
@@ -79,24 +81,24 @@ impl<'a> BVH<'a> {
                     }
                 }
             } else {
-                if stack_top < STACK_SIZE as i64 - 1 {
-                    stack[stack_top as usize] = node.left_node;
-                    stack_top += 1;
-                }
-                if stack_top < STACK_SIZE as i64 - 1 {
+                if stack_top < STACK_SIZE as i64 {
                     stack[stack_top as usize] = node.left_node + 1;
                     stack_top += 1;
                 }
+                if stack_top < STACK_SIZE as i64 {
+                    stack[stack_top as usize] = node.left_node;
+                stack_top += 1;
             }
         }
-
-        hit
     }
+
+    hit
+}
 }
 
 fn intersect_aabb(ray: &Ray, t: f64, b_min: Vec3, b_max: Vec3) -> bool {
-    let tx1 = (b_min.x - ray.origin.x) / ray.direction.x;
-    let tx2 = (b_max.x - ray.origin.x) / ray.direction.x;
+let tx1 = (b_min.x - ray.origin.x) / ray.direction.x;
+let tx2 = (b_max.x - ray.origin.x) / ray.direction.x;
     let tmin = min_f64(tx1, tx2);
     let tmax = max_f64(tx1, tx2);
     let ty1 = (b_min.y - ray.origin.y) / ray.direction.y;
@@ -111,12 +113,9 @@ fn intersect_aabb(ray: &Ray, t: f64, b_min: Vec3, b_max: Vec3) -> bool {
     tmax >= tmin && tmin < t && tmax > 0.0
 }
 
-/*
- * May have to transfer over to u64 instead of usize.
- */
 pub fn build_bvh(stream: &Arc<CudaStream>, tris: &Vec<Triangle>) -> (DeviceBuffer<BVHNode>, DeviceBuffer<usize>, DeviceBuffer<Triangle>) {
     let root_node_idx = 0;
-    let mut nodes_used = 0;
+    let mut nodes_used = 1;
     let mut bvh_nodes: Vec<BVHNode> = Vec::with_capacity(tris.len() * 2 - 1);
     let mut tri_indices: Vec<usize> = (0..tris.len()).collect();
 
@@ -125,7 +124,7 @@ pub fn build_bvh(stream: &Arc<CudaStream>, tris: &Vec<Triangle>) -> (DeviceBuffe
     root.left_node = 0;
     root.first_tri_idx = 0;
     root.tri_count = tris.len();
-    update_node_bounds(root_node_idx, &mut bvh_nodes, tris);
+    update_node_bounds(root_node_idx, &mut bvh_nodes, &tri_indices, tris);
     subdivide(root_node_idx, &mut nodes_used, &mut bvh_nodes, tris, &mut tri_indices);
 
     let nodes_dev = DeviceBuffer::from_host(stream, &bvh_nodes).unwrap();
@@ -135,14 +134,14 @@ pub fn build_bvh(stream: &Arc<CudaStream>, tris: &Vec<Triangle>) -> (DeviceBuffe
     (nodes_dev, tri_indices_dev, tris_dev)
 }
 
-fn update_node_bounds(node_idx: usize, bvh_nodes: &mut Vec<BVHNode>, tris: &Vec<Triangle>) {
+fn update_node_bounds(node_idx: usize, bvh_nodes: &mut Vec<BVHNode>, tri_indices: &Vec<usize>, tris: &Vec<Triangle>) {
     let node = &mut bvh_nodes[node_idx];
     node.aabb_min = Vec3::new(f64::MAX, f64::MAX, f64::MAX);
     node.aabb_max = Vec3::new(f64::MIN, f64::MIN, f64::MIN);
 
-    let first = node.first_tri_idx;
     for i in 0..node.tri_count {
-        let leaf_tri = tris[first + i];
+        let leaf_tri_idx = tri_indices[node.first_tri_idx + i];
+        let leaf_tri = tris[leaf_tri_idx];
         node.aabb_min = fminf(node.aabb_min, leaf_tri.vertice1);
         node.aabb_min = fminf(node.aabb_min, leaf_tri.vertice2);
         node.aabb_min = fminf(node.aabb_min, leaf_tri.vertice3);
@@ -167,18 +166,18 @@ fn subdivide(node_idx: usize, nodes_used: &mut usize, bvh_nodes: &mut Vec<BVHNod
         if tris[tri_indices[i]].origin[axis] < split_pos {
             i += 1;
         } else {
-            tri_indices.swap(i, j - 1);
-            j -= 1;
+            tri_indices.swap(i, j);
+            j -= 1
         }
     }
 
     let left_count = i - bvh_nodes[node_idx].first_tri_idx;
     if left_count == 0 || left_count == bvh_nodes[node_idx].tri_count { return }
 
-    *nodes_used += 1;
     let left_child_idx = *nodes_used;
     *nodes_used += 1;
     let right_child_idx = *nodes_used;
+    *nodes_used += 1;
 
     bvh_nodes.insert(left_child_idx, BVHNode::empty());
     bvh_nodes.insert(right_child_idx, BVHNode::empty());
@@ -190,8 +189,8 @@ fn subdivide(node_idx: usize, nodes_used: &mut usize, bvh_nodes: &mut Vec<BVHNod
     bvh_nodes[node_idx].left_node = left_child_idx;
     bvh_nodes[node_idx].tri_count = 0;
 
-    update_node_bounds(left_child_idx, bvh_nodes, tris);
-    update_node_bounds(right_child_idx, bvh_nodes, tris);
+    update_node_bounds(left_child_idx, bvh_nodes, tri_indices, tris);
+    update_node_bounds(right_child_idx, bvh_nodes, tri_indices, tris);
 
     subdivide(left_child_idx, nodes_used, bvh_nodes, tris, tri_indices);
     subdivide(right_child_idx, nodes_used, bvh_nodes, tris, tri_indices);
